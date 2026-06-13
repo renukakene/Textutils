7fdf44a96aac53d4aedc364e9cc04782828b0fd7
renukakene <renukakene2004@gmail.com>
2026-06-13 17:11:36 +1000

    Added navbar and textform components

---
+++ src/components/Textform.jsx
@@ -0,0 +1,40 @@
+import React,{useState}from 'react'
+
+export default function Textform(props) {
+
+  const handleUpClick = () => {
+    setText(text.toUpperCase());
+  }
+
+  const handleLoClick = () => {
+    setText(text.toLowerCase());
+  }
+
+  const handleOnChange = (e) => {
+    setText(e.target.value);
+  }
+  const [text, setText] = useState("enter text here");
+
+  return (
+      <> 
+        <div className="container">
+          <div className="mb-3">
+            <h1>{props.heading}</h1>
+            <textarea className="form-control" onChange={handleOnChange} id="mybox" rows="8" value={text} ></textarea>
+          </div>
+          <button className="btn btn-dark" onClick={handleUpClick}>Convert to Uppercase</button>
+          <button className="btn btn-dark mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+          <button className="btn btn-secondary" onClick={() => { setText(""); }}>Clear Text</button>
+        </div>
+
+        <div className="container my-3">
+          <h2>Your text summary</h2>
+          <p>{text.split(" ").filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
+          <p>{0.008 * text.split(" ").length} Minutes read</p>
+          <h2>Preview</h2>
+          <p>{text}</p>
+        </div>
+      </> 
+  )
+}
+
